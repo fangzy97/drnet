bad0fa69405875b997afae9a884008b62aac0ab5
lepetit <1299012718@qq.com>
2022-05-06 06:21:19 +0000

    添加使用原来结构+FEM+pretrain+fix bn

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,5 @@
 from . import *
 
-__all__ = ['asgnet', 'drnet', 'drnetv2', 'drnetv3',
+__all__ = ['asgnet', 'drnet', 'drnetv2', 'drnetv3', 'drnetv4', 'drnetv3_iter', 'drnetv3_iter2',
+           'drnet_ori_fem',
            'pspnet']
